2c31c6cc4894debe1a70e94cc50bf17276015162
Imran <imrantipu90@yahoo.com>
2023-01-11 20:29:06 +0100

    a banner added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="mt-5 ">
+      <div className="hero bg-base-200 rounded-xl ">
+        <div className="hero-content flex-col lg:flex-row-reverse ">
+          <div className="mockup-phone border-primary">
+            <div className="camera"></div>
+            <div className="display">
+              <div className="artboard artboard-demo phone-1">MobileHunt</div>
+            </div>
+          </div>
+          <div >
+            <h1 className="text-5xl font-bold">
+              Get more money for your used smartphone
+            </h1>
+            <p className="py-6">
+            Selling your smartphone has just become… handy.
+            <span className="font-bold">MobileHunt</span> gets your smartphone off your hands pronto and at top dollar!
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -30,7 +30,10 @@ const Navbar = () => {
             >
               {/* navbar middle for full view */}
               <li>
-                <Link to="/">Home</Link>
+              <button>
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+    <Link className="link link-hover" to="/">Home</Link>
+  </button>
               </li>
             </ul>
           </div>
@@ -41,7 +44,11 @@ const Navbar = () => {
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <Link to="/">Home</Link>
+            <button>
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+    <Link className="link link-hover" to="/">Home</Link>
+  </button>
+            
             </li>
           </ul>
         </div>
@@ -68,7 +75,7 @@ const Navbar = () => {
               </label>
               <div
                 tabIndex={0}
-                className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
+                className="mt-3 card card-compact dropdown-content w-52 bg-base-300 shadow"
               >
                 <div className="card-body">
                   <span className="font-bold text-lg">8 Items</span>
@@ -84,7 +91,7 @@ const Navbar = () => {
             <div className="dropdown dropdown-end">
               <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full">
-                  <img src="https://placeimg.com/80/80/people" />
+                  <img src="https://placeimg.com/80/80/people" alt= "" />
                 </div>
               </label>
               <ul
@@ -92,16 +99,7 @@ const Navbar = () => {
                 className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
               >
                 <li>
-                  <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </a>
-                </li>
-                <li>
-                  <a>Settings</a>
-                </li>
-                <li>
-                  <a>Logout</a>
+                  <Link to = "/">Logout</Link>
                 </li>
               </ul>
             </div>
