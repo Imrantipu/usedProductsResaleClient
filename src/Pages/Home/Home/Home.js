84b67e89b8e094fce984e29330ef08f4b116a917
Imran <imrantipu90@yahoo.com>
2023-01-12 08:14:20 +0100

    home extra section made

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import HomeSection from '../HomeSection/HomeSection';
 
 const Home = () => {
     return (
         <div className='mx-5'>
            <Banner></Banner>
+           <HomeSection></HomeSection>
         </div>
     );
 };
