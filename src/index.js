1939820bcca34c51f79c1217a1a6203e6c1d75be
Imran <imrantipu90@yahoo.com>
2023-01-12 14:04:40 +0100

    auth provider set up done

--- src/index.js
+++ src/index.js
@@ -8,14 +8,17 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import AuthProvider from './contexts/AuthProvider';
 
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+   <QueryClientProvider client={queryClient}>
+      <AuthProvider>
         <App />
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
